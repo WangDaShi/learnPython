d10fbedd7c7aef7d75c3e15cf4a36062f9193bfb
wangbei <wangbei99@hotmail.com>
2020-07-31 12:09:28 +0800

    调整格式

--- client.py
+++ client.py
@@ -5,6 +5,16 @@ from aioconsole import ainput
 HOST = '127.0.0.1'  # 服务器的主机名或者 IP 地址
 PORT = 65432        # 服务器使用的端口
 
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+    print('连接已建立')
+    loop = asyncio.get_event_loop()
+    #loop.create_task(asyncio.start_server(handle_receive, HOST, PORT))
+    loop.create_task(handle_receive(client))
+    loop.create_task(handle_send(client))
+    loop.run_forever()
+
 async def handle_receive(conn):
     print('started')
     while True:
@@ -22,27 +32,4 @@ async def handle_send(conn):
         await loop.sock_sendall(conn,str.encode('utf-8'))
     print('done')
 
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
-print('连接已建立')
-loop = asyncio.get_event_loop()
-#loop.create_task(asyncio.start_server(handle_receive, HOST, PORT))
-loop.create_task(handle_receive(client))
-loop.create_task(handle_send(client))
-loop.run_forever()
-
-
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     print('开始发送数据')
-#     while(True):
-#         print('me:')
-#         str = input()
-#         s.sendall(str.encode('utf-8'))
-#         data = s.recv(1024)
-#         print('you:')
-#         print(data.decode('utf-8'))
-
-# print('退出')
+main()
--- server.py
+++ server.py
@@ -5,6 +5,18 @@ from aioconsole import ainput
 HOST = '127.0.0.1'  # 标准的回环地址 (localhost)
 PORT = 65432        # 监听的端口 (非系统级的端口: 大于 1023)
 
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(1)
+    # server.setblocking(False)
+    conn,_ = server.accept()
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(handle_receive(conn))
+    loop.create_task(handle_send(conn))
+    loop.run_forever()
+
 async def handle_receive(conn):
     print('started')
     while True:
@@ -22,14 +34,4 @@ async def handle_send(conn):
         await loop.sock_sendall(conn,str.encode('utf-8'))
     print('done')
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(1)
-#server.setblocking(False)
-conn,addr = server.accept()
-
-loop = asyncio.get_event_loop()
-#loop.create_task(asyncio.start_server(handle_receive, HOST, PORT))
-loop.create_task(handle_receive(conn))
-loop.create_task(handle_send(conn))
-loop.run_forever()
+main()
